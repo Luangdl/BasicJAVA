e940e72c91e429b8b5d45ad8baee2f8a70b711fb
Luangdl <luam_gdl@hotmail.com>
2024-08-01 15:23:58 -0300

    alteracao da diferenca

--- segundoProjetoGit/src/pacote1/SegundaClasse.java
+++ segundoProjetoGit/src/pacote1/SegundaClasse.java
@@ -3,6 +3,7 @@ package pacote1;
 public class SegundaClasse {
 
     public static void main(String[] args) {
-        System.out.println("sei la ");
+        System.out.println("comparando diferencas ");
+        System.out.println("era outra coisa");
     }
 }
